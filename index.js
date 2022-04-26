856495f470625c575143a66bd7a1027ebc732916
Nitin Kumar <snitin315@gmail.com>
2022-04-26 07:55:03 +0000

    fix: ci

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const gitHubActions = require('@actions/github');
 
 const context = gitHubActions.context;
 console.log(context);
-console.log(context.playload.commits);
+console.log(context.payload.commits);
