d880f74475c38aec5232a5fa72231a54e2d4985e
Nitin Kumar <snitin315@gmail.com>
2022-04-26 08:02:42 +0000

    fix: ci

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ const gitHubActions = require('@actions/github');
 const context = gitHubActions.context;
 console.log(context);
 console.log(context.payload.pull_request.base);
+console.log("commits",context.payload.commits);
