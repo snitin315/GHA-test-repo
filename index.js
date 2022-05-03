70b24a7d721e62462fc31eac4586c739a663795a
Nitin Kumar <snitin315@gmail.com>
2022-05-03 13:20:01 +0000

    test

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const gitHubActions = require('@actions/github');
 const gitHubActionsCore = require('@actions/core');
 
-console.log(gitHubActions.context);
 const getCommentBody = (isBuildSuccessful) => {
   const buildStatus = isBuildSuccessful ? '✅ Ready' : '❌ Failed';
   const buildCommit = gitHubActions.context.payload.after || gitHubActions.context.sha;
@@ -15,15 +14,6 @@ const getCommentBody = (isBuildSuccessful) => {
 
   return commentBody.join('\n');
 };
-
-function findCommentPredicate(inputs, comment) {
-  return (
-    (inputs.commentAuthor && comment.user ? comment.user.login === inputs.commentAuthor : true) &&
-    (inputs.bodyIncludes && comment.body
-      ? comment.body.includes(inputs.bodyIncludes) && comment.body.includes(process.env.APP_NAME)
-      : true)
-  );
-}
 
 async function findComment(inputs) {
   const octokit = gitHubActions.getOctokit(inputs.token);
@@ -40,7 +30,10 @@ async function findComment(inputs) {
     parameters,
   )) {
     // Search each page for the comment
-    const requiredComment = comments.find((comment) => findCommentPredicate(inputs, comment));
+    const requiredComment = comments.find(
+      (comment) =>
+        comment.body.includes('The latest updates on your project') && comment.body.includes(process.env.APP_NAME),
+    );
     if (requiredComment) return requiredComment;
   }
   return undefined;
@@ -52,7 +45,6 @@ async function getBuildInfo() {
       token: process.env.TOKEN,
       buildStatus: process.env.BUILD_STATUS === 'true',
       issueNumber: process.env.ISSUE_NUMBER || gitHubActions.context.payload.number,
-      bodyIncludes: 'The latest updates on your project',
     };
 
     if (typeof inputs.token === 'undefined' || typeof inputs.buildStatus === 'undefined') {
@@ -91,10 +83,4 @@ async function getBuildInfo() {
   }
 }
 
-if (gitHubActions.context.eventName === "pull_request") {
-  getBuildInfo();
-}
-
-
-
-
+getBuildInfo();
