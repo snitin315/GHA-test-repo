a1e7f8c502578da23fe74da073c41a3660c20909
Nitin Kumar <snitin315@gmail.com>
2022-06-06 01:09:23 +0000

    test: commit on push

--- index.js
+++ index.js
@@ -3,94 +3,104 @@ const gitHubActionsCore = require('@actions/core');
 
 //console.log(gitHubActions.context);
 
-const getCommentBody = (status) => {
-  const buildStatus = status === 'success' ? '✅ Ready' : '❌ Failed';
-  const buildCommit = gitHubActions.context.payload.after || gitHubActions.context.sha;
-
-  const commentBody = [
-    '**The latest updates on your project.**',
-    '|App Name|Build Status|Build Version|Build Commit|',
-    '|---|---|---|---|',
-    `|${process.env.APP_NAME}|${buildStatus}|${process.env.VERSION}|${buildCommit}|`,
-  ];
-
-  return commentBody.join('\n');
-};
-
-async function findComment(inputs) {
-  const octokit = gitHubActions.getOctokit(inputs.token);
-  const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
-
-  const parameters = {
-    owner,
-    repo,
-    issue_number: inputs.issueNumber,
-  };
-
-  for await (const { data: comments } of octokit.paginate.iterator(
-    octokit.rest.issues.listComments,
-    parameters,
-  )) {
-    // Search each page for the comment
-    const requiredComment = comments.find(
-      (comment) =>
-        comment.body.includes('The latest updates on your project') &&
-        comment.body.includes(process.env.APP_NAME),
-    );
-    if (requiredComment) return requiredComment;
-  }
-  return undefined;
+const inputs = {
+  token: process.env.GITHUB_TOKEN,
+  buildStatus: process.env.BUILD_STATUS,
+  issueNumber: process.env.ISSUE_NUMBER || gitHubActions.context.payload.number,
 }
 
-async function getBuildInfo() {
-  try {
-    const inputs = {
-      token: process.env.GITHUB_TOKEN,
-      buildStatus: process.env.BUILD_STATUS,
-      issueNumber: process.env.ISSUE_NUMBER || gitHubActions.context.payload.number,
-    };
+const octokit = gitHubActions.getOctokit(inputs.token);
+const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
+const pulls = await octokit.request('GET /repos/{owner}/{repo}/commits/{commit_sha}/pulls', {
+  owner,
+  repo,
+  commit_sha: gitHubActions.context.payload.after || gitHubActions.context.sha
+})
 
-    if (typeof inputs.token === 'undefined' || typeof inputs.buildStatus === 'undefined') {
-      gitHubActionsCore.setFailed("Missing either 'TOKEN' or 'BUILD_STATUS'.");
-      return;
-    }
+console.log("pulls->", pulls)
 
-    const comment = await findComment(inputs);
-    const octokit = gitHubActions.getOctokit(inputs.token);
-    const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
+// const getCommentBody = (status) => {
+//   const buildStatus = status === 'success' ? '✅ Ready' : '❌ Failed';
+//   const buildCommit = gitHubActions.context.payload.after || gitHubActions.context.sha;
 
-    const pulls = await octokit.request('GET /repos/{owner}/{repo}/commits/{commit_sha}/pulls', {
-      owner: 'OWNER',
-      repo: 'REPO',
-      commit_sha: gitHubActions.context.payload.after || gitHubActions.context.sha
-    })
+//   const commentBody = [
+//     '**The latest updates on your project.**',
+//     '|App Name|Build Status|Build Version|Build Commit|',
+//     '|---|---|---|---|',
+//     `|${process.env.APP_NAME}|${buildStatus}|${process.env.VERSION}|${buildCommit}|`,
+//   ];
 
-    console.log("pulls->", pulls)
-    if (comment) {
-      const commentId = comment.id.toString();
-      await octokit.rest.issues.updateComment({
-        owner,
-        repo,
-        comment_id: commentId,
-        body: getCommentBody(inputs.buildStatus),
-      });
-      gitHubActionsCore.info(`Updated comment id '${commentId}'.`);
-      gitHubActionsCore.setOutput('comment-id', commentId);
-    } else {
-      const { data: commentData } = await octokit.rest.issues.createComment({
-        owner,
-        repo,
-        issue_number: inputs.issueNumber,
-        body: getCommentBody(inputs.buildStatus),
-      });
-      gitHubActionsCore.info(
-        `Created comment id '${commentData.id}' on issue '${inputs.issueNumber}'.`,
-      );
-      gitHubActionsCore.setOutput('comment-id', commentData.id);
-    }
-  } catch (error) {
-    gitHubActionsCore.setFailed(error.message);
-  }
-}
+//   return commentBody.join('\n');
+// };
 
-getBuildInfo();
+// async function findComment(inputs) {
+//   const octokit = gitHubActions.getOctokit(inputs.token);
+//   const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
+
+//   const parameters = {
+//     owner,
+//     repo,
+//     issue_number: inputs.issueNumber,
+//   };
+
+//   for await (const { data: comments } of octokit.paginate.iterator(
+//     octokit.rest.issues.listComments,
+//     parameters,
+//   )) {
+//     // Search each page for the comment
+//     const requiredComment = comments.find(
+//       (comment) =>
+//         comment.body.includes('The latest updates on your project') &&
+//         comment.body.includes(process.env.APP_NAME),
+//     );
+//     if (requiredComment) return requiredComment;
+//   }
+//   return undefined;
+// }
+
+// async function getBuildInfo() {
+//   try {
+//     const inputs = {
+//       token: process.env.GITHUB_TOKEN,
+//       buildStatus: process.env.BUILD_STATUS,
+//       issueNumber: process.env.ISSUE_NUMBER || gitHubActions.context.payload.number,
+//     };
+
+//     if (typeof inputs.token === 'undefined' || typeof inputs.buildStatus === 'undefined') {
+//       gitHubActionsCore.setFailed("Missing either 'TOKEN' or 'BUILD_STATUS'.");
+//       return;
+//     }
+
+//     const comment = await findComment(inputs);
+//     const octokit = gitHubActions.getOctokit(inputs.token);
+//     const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
+
+    
+//     if (comment) {
+//       const commentId = comment.id.toString();
+//       await octokit.rest.issues.updateComment({
+//         owner,
+//         repo,
+//         comment_id: commentId,
+//         body: getCommentBody(inputs.buildStatus),
+//       });
+//       gitHubActionsCore.info(`Updated comment id '${commentId}'.`);
+//       gitHubActionsCore.setOutput('comment-id', commentId);
+//     } else {
+//       const { data: commentData } = await octokit.rest.issues.createComment({
+//         owner,
+//         repo,
+//         issue_number: inputs.issueNumber,
+//         body: getCommentBody(inputs.buildStatus),
+//       });
+//       gitHubActionsCore.info(
+//         `Created comment id '${commentData.id}' on issue '${inputs.issueNumber}'.`,
+//       );
+//       gitHubActionsCore.setOutput('comment-id', commentData.id);
+//     }
+//   } catch (error) {
+//     gitHubActionsCore.setFailed(error.message);
+//   }
+// }
+
+// getBuildInfo();
